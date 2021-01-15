c9b178d62b9966ea2b3065ff0f57ff1af2eff0f2
Romina Trazzi <romina.trazzi@posta.it>
2021-01-15 10:56:53 +0100

    Commit popolamento array milestone 1 grezzo
    Dati in arrivo dal database ma visualizzati ancora in modo imperfetto

--- milestone 1/assets/js/main.js
+++ milestone 1/assets/js/main.js
@@ -1,5 +1,12 @@
 
-
+/* Milestone 1:
+Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. 
+Possiamo, cliccando il bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente. 
+Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+1. Titolo
+2. Titolo Originale 
+3. Lingua
+4. Voto */
 
 let app = new Vue ({
     el: "#app",
@@ -9,15 +16,15 @@ let app = new Vue ({
     },
  
     methods: {
-        onClickButton: function(find) {
-            axios.get("https://api.themoviedb.org/3/search/movie?api_key=e711a9a7f93bd4ad57163f1845d0059f&query=userRequest")
+        onClickButton: function() {
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e711a9a7f93bd4ad57163f1845d0059f&language=itIT&query=${this.userRequest}&include_adult=false`)
             .then(response => {  
                 // console.log(response);
                 this.movies = response.data.results;
                 console.log(this.movies);
             })
             .catch(error => {
-                console.log("Error: " + error) // gestiamo l'errore
+                console.log("Error: " + error) 
             })
         }
 
