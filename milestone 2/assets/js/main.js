4f43b01ee67f4a12a97968c70f96b499a5ade996
Romina Trazzi <romina.trazzi@posta.it>
2021-01-15 12:07:31 +0100

    Commit inizio milestone 2

---
+++ milestone 2/assets/js/main.js
@@ -0,0 +1,51 @@
+/* Milestone 2:
+Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, 
+così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, 
+lasciando le restanti vuote (troviamo le icone in FontAwesome).
+Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, 
+gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+
+Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+Qui un esempio di chiamata per le serie tv: 
+https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs */
+
+
+let app = new Vue ({
+    el: "#app",
+    data: {
+        userRequest: "",
+        movies: [],
+        tvSeries: []
+    },
+ 
+    methods: {
+        onClickButton: function() {
+            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=e711a9a7f93bd4ad57163f1845d0059f&language=it_IT&query=${this.userRequest}&include_adult=false`) // richiesta film
+            .then(response => {  
+                this.movies = response.data.results;
+            })
+            .catch(error => {
+                console.log("Error: " + error) 
+            })
+
+            for (let i = 0; i < this.movies.length; i++) {
+                let stars = this.movies[i].vote_average;
+                stars = Math.ceil(stars);
+                console.log(stars);    
+            };
+
+            
+        }
+    },
+    
+    mounted(){
+        
+    }
+})
+
+
+// axios.get(`https://api.themoviedb.org/3/search/tv?api_key=e711a9a7f93bd4ad57163f1845d0059f&language=it_IT&query=${this.userRequest}&include_adult=false`) // richiesta serie tv       
+
+
+
